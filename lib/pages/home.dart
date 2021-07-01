a8f8ec839db37d7446542d25ee35b5dc0479dc45
MounirBouhou <52429781+MounirBouhou@users.noreply.github.com>
2021-06-30 22:15:04 -0400

    Updated initState to check for internet connectivity before calling the NewGamesBloc.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,6 +2,8 @@
 
 import 'package:cnargames/blocs/new_games_bloc.dart';
 import 'package:cnargames/models/games.dart';
+import 'package:cnargames/services/app_services.dart';
+import 'package:cnargames/utils/snacbar.dart';
 import 'package:cnargames/widgets/last_games.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -13,20 +15,32 @@ class Home extends StatefulWidget{
 }
 
 class _HomeState extends State<Home> {
+  ScrollController? _controller;
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
 
   @override
   void initState() {
     super.initState();
-    print("----- Init State");
-    Future.delayed(Duration(seconds: 1)).then((value) => context.read<NewGamesBloc>().getGames());
+    // Scroller
+    _controller = ScrollController(initialScrollOffset: 0.0, keepScrollOffset: true);
+   // _controller!.addListener(_scrollListner)
+    // Call the Games
+    AppServices().checkInternet().then(
+        (hasInternet){
+          if (hasInternet!){
+            Future.delayed(Duration(seconds: 0)).then((value) => context.read<NewGamesBloc>().getGames());
+          } else {
+            openSnacbar(scaffoldKey, "مشكلة في الانترنت!");
+          }
+        });
   }
 
   Future _onRefresh() async {
     print("----- _onRefresh");
     context.read<NewGamesBloc>().getGames();
   }
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +50,9 @@ class _HomeState extends State<Home> {
 
       body: SafeArea(
         child: RefreshIndicator(onRefresh: () async => await _onRefresh(),
-            child: SingleChildScrollView(  // ++++++++++++++++++++
+            child: SingleChildScrollView(
+              physics: ClampingScrollPhysics(),
+              controller: _controller,// ++++++++++++++++++++
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -44,24 +60,30 @@ class _HomeState extends State<Home> {
                     title: Text("CN"),
                   ),
 
-                   LastGames(
-                       gamz: context.watch<NewGamesBloc>().games, scaffoldKey: scaffoldKey
-                   ),
+                  LastGames( gamz: context.watch<NewGamesBloc>().games, scaffoldKey: scaffoldKey ),
+
+                  // Opacity(
+                  //   opacity: context.watch<NewGamesBloc>().loading == true ? 1.0 : 0.0,
+                  //   child: Container(
+                  //       height: 60,
+                  //       child: Row(
+                  //         mainAxisAlignment: MainAxisAlignment.center,
+                  //         children: [
+                  //           CupertinoActivityIndicator(),
+                  //           SizedBox(width: 10,),
+                  //           Text('Loading contents')
+                  //         ],
+                  //       )
+                  //   ),
+                  // )
+
+
                 ],
               ),
             )
-
         ),
-
       ),
-
     );
-
-
-
-
-
-
   } //// build End
 }
 
