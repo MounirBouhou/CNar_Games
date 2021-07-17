164a3aa8d927e49353afc27adfbaa506492763c9
MounirBouhou <52429781+MounirBouhou@users.noreply.github.com>
2021-07-17 02:07:50 -0400

    Some properties are added to the snacbar creator.

--- lib/utils/snacbar.dart
+++ lib/utils/snacbar.dart
@@ -1,39 +1,30 @@
+import 'package:cnargames/colors/custom_colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void openSnacbar(scaffoldKey, snackMessage){
+void openSnacbar(scaffoldKey, snackMessage, bColor, ic){
   scaffoldKey.currentState.showSnackBar(
-      SnackBar(
-    // Key? key,
-    // required this.content,
-    // this.backgroundColor,
-    // this.elevation,
-    // this.margin,
-    // this.padding,
-    // this.width,
-    // this.shape,
-    // this.behavior,
-    // this.action,
-    // this.duration = _snackBarDisplayDuration,
-    // this.animation,
-    // this.onVisible,
-    duration: Duration(seconds: 2),
-    content: Container(
-      alignment: Alignment.centerLeft,
-      height: 60,
-      child: Text(snackMessage, style: TextStyle(fontSize: 14),
-    ),
-  ),
-  action: SnackBarAction(
-  // this.textColor,
-  // this.disabledTextColor,
-  // required this.label,
-  // required this.onPressed,
-  label: 'OK',
-  textColor: Colors.lightBlue,
-  onPressed: () {  },
-
-
-  ),));
-
-
+        SnackBar( backgroundColor: bColor, duration: Duration(seconds: 3),
+        content:
+        Directionality(
+          textDirection: TextDirection.rtl,
+             child: Row(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                 children: [
+                   Text(snackMessage, style: TextStyle(fontSize: 20, color: cColors().gray)),
+                   Icon(ic, size: 30, color: cColors().gray,),
+                 ],
+            ),
+            ),
+            //action: SnackBarAction(
+            //// this.textColor,
+            //// this.disabledTextColor,
+            //// required this.label,
+            //// required this.onPressed,
+            //label: 'OK',
+            //textColor: Colors.lightBlue,
+            //onPressed: () { },
+            //),
+        ),
+  );
 }
