cd5e5b7b3ebc7999c7209203fd80ef08d4c8bc8f
MounirBouhou <52429781+MounirBouhou@users.noreply.github.com>
2021-07-17 02:09:16 -0400

    wp_config.dart is updated

--- lib/config/wp_config.dart
+++ lib/config/wp_config.dart
@@ -3,6 +3,6 @@
 class WpConfig {
 
   final String webURL = "https://cn-arabia.com";
-  final String randomGameFeaturedImage = "https://innov8tiv.com/wp-content/uploads/2017/10/blank-screen-google-play-store-1280x720.png";
+  final String randomGameFeaturedImage = 'assets/images/splash.png';
 
 }
