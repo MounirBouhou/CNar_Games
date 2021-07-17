0937d5e803ffa9112445b3d91f15586d43d1d9be
MounirBouhou <52429781+MounirBouhou@users.noreply.github.com>
2021-07-17 01:56:55 -0400

    Drawer is added

---
+++ lib/widgets/drawer.dart
@@ -0,0 +1,89 @@
+import 'package:cnargames/Colors/custom_colors.dart';
+import 'package:cnargames/config/config.dart';
+import 'package:cnargames/services/app_services.dart';
+import 'package:cnargames/utils/next_screen.dart';
+import 'package:cnargames/widgets/policy.dart';
+import 'package:flutter/material.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Directionality(textDirection: TextDirection.rtl,
+    child: Drawer(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+                height: 260,
+                width: double.infinity,
+                color: cColors().yellow,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(bottom: 3),
+                      child: Image(
+                          height: 100,
+                          width: 100,
+                          fit: BoxFit.contain,
+                          image: AssetImage(Config().splash)),
+                    ),
+                    Text(Config().appName)
+
+                  ],
+                )
+            ),
+            Container(
+              padding: EdgeInsets.all(15),
+              child: Column(
+                children: [
+                  ListTile(
+                    isThreeLine: false,
+                    contentPadding: EdgeInsets.all(0),
+                    leading: Icon(Icons.email_outlined, size: 22,),
+                    horizontalTitleGap: 5,
+                    title: Text(
+                      'اتصل بنا',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    onTap: (){
+                      Navigator.pop(context);
+                      AppServices().openEmailSupport();
+                    },
+                  ),
+                  ListTile(
+                    contentPadding: EdgeInsets.all(0),
+                    leading: Icon(Icons.policy_outlined, size: 22,),
+                    horizontalTitleGap: 5,
+                    title: Text(
+                      'سياسة الخصوصية',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    onTap: (){
+                      nextScreenPopup(context, Policy());
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              color: Colors.grey[600],
+            ),
+          ],
+        ),
+      ),
+    )
+    );
+  }
+}
