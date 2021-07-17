f8299069e71a0b3d0441a7185f4a95ccb9f899dd
MounirBouhou <52429781+MounirBouhou@users.noreply.github.com>
2021-07-17 02:05:05 -0400

    Policy page is added.

---
+++ lib/widgets/policy.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+class Policy extends StatefulWidget{
+   const Policy({Key? key})  :  super(key: key);
+
+  @override
+  _Policy  createState() => _Policy();
+}
+
+class _Policy extends State<Policy> {
+
+  @override
+  Widget build(BuildContext context) {
+      return Directionality(
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('سياسة الخصوصية'),
+              elevation: 0,
+            ),
+
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: FutureBuilder(
+              future: Future.delayed(Duration(milliseconds: 150)).then((value) =>
+              rootBundle.loadString('assets/privacy_policy.md')),
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                if (snapshot.hasData){
+                  return Markdown(
+                    data: snapshot.data,
+                  );
+                }
+
+                return Center(child: CircularProgressIndicator(),);
+              },
+
+            ),
+          ),
+        ),
+
+        ),
+      );
+  }
+}
