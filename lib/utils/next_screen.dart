c4a780449e619e747b4919e44931357add585a54
MounirBouhou <52429781+MounirBouhou@users.noreply.github.com>
2021-07-17 02:02:49 -0400

    nextScreenReplace, nextScreenPopup are added

--- lib/utils/next_screen.dart
+++ lib/utils/next_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,4 +6,15 @@ void nextScreen(context, page){
   Navigator.push(context, MaterialPageRoute(
       builder: (context) => page
   ));
+}
+
+void nextScreenReplace(context, page){
+  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => page));
+}
+
+void nextScreenPopup (context, page){
+  Navigator.push(context, MaterialPageRoute(
+      fullscreenDialog: true,
+      builder: (context) => page),
+  );
 }
