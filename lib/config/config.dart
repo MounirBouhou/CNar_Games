c4b24d40446de9c786a53b78d1ffd5453ce00305
MounirBouhou <52429781+MounirBouhou@users.noreply.github.com>
2021-07-17 01:49:31 -0400

    updated

---
+++ lib/config/config.dart
@@ -0,0 +1,14 @@
+
+
+
+
+class Config {
+
+  final String appName = 'إلعب معنا!';
+  final String supportEmail = 'mounir.bouhoo@gmail.com';
+
+  final String appIcon = 'assets/images/icon.png';
+  final String logo = 'assets/images/logo.png';
+  final String splash = 'assets/images/splash.png';
+  final String noNet = 'assets/images/noNet.png';
+}
