6e76e19ecda369dda5840a60f28c760232d1541b
MounirBouhou <52429781+MounirBouhou@users.noreply.github.com>
2021-07-17 01:56:07 -0400

    checking network status then notify the listeners.

---
+++ lib/services/connectivity_provider.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+import 'package:connectivity/connectivity.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+class ConnectivityProvider extends ChangeNotifier{
+
+  Connectivity _connectivity = new Connectivity();
+  bool? _isOnline;
+  bool get isOnline => _isOnline!;
+
+  startMonitoring() async {
+      await initConnectivity();
+      _connectivity.onConnectivityChanged.listen((result) async {
+        if (result == ConnectivityResult.none){
+          _isOnline = false;
+          notifyListeners();
+        } else {
+           await updateConnectionStatus().then((bool? isConnected){
+             _isOnline = isConnected!;
+              notifyListeners();
+           });
+        }
+      });
+
+
+  }
+
+  Future<void> initConnectivity() async{
+    try{
+      var status = await _connectivity.checkConnectivity();
+
+      if (status == ConnectivityResult.none){
+          _isOnline = false;
+          notifyListeners();
+      } else {
+        _isOnline = true;
+        notifyListeners();
+      }
+    } on PlatformException catch (e){
+       print("P = " + e.toString());
+    }
+  }
+
+
+  Future<bool?> updateConnectionStatus() async {
+    bool? isConnected;
+    try{
+      final List<InternetAddress> r = await InternetAddress.lookup("google.com");
+      if (r.isNotEmpty && r[0].rawAddress.isNotEmpty){
+        isConnected = true;
+      }
+    } on SocketException catch (_) {
+         isConnected = false;
+    }
+
+    return isConnected;
+  }
+
+
+
+}
