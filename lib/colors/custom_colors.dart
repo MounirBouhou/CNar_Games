042adfd559abc01f3e7e1e3626a43445c7873ae2
MounirBouhou <52429781+MounirBouhou@users.noreply.github.com>
2021-08-18 10:31:19 -0400

    colors updated

---
+++ lib/colors/custom_colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+class cColors {
+
+  final  gray = Color(0xfff5f5f5);
+  final  blue = Color(0xff68cbe9);
+  final  blueDark = Color(0xff00B0F6);
+  final  pink = Color(0xffe085d2);
+  final  pinkDark = Color(0xffFF008E);
+  final  yellow = Color(0xffe0db85);
+  final  yellowDark = Color(0xffFFCE00);
+  final  error = Color(0xffbd8992);
+  final  pass = Color(0xff7fb88a);
+  final  noInternet = Color(0xffDB5F5E);
+//final  lpi = Color(0xFFe25dab);
+}
